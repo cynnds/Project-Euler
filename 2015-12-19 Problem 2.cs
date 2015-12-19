0d3164f1f9c9cdc280a85053af246ac38b4dd1b7
Cynthia Subakat <cynthia.subakat@uts.edu.au>
2015-12-19 18:25:44 +1100

    problem 2

---
+++ 2015-12-19 Problem 2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int total = 0, x=0, y=1, z=0;
+
+                while (y <= 4000000)
+                {
+                    z = x + y;
+                    x = y ;
+                    y = z;
+
+                    if (y%2 == 0)
+                        total += y;
+                }
+
+                Console.WriteLine(total);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
